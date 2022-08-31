77fb22ca1aaf3d5cde950785f25d95cb04f9627b
alexsteeve <alexsteeve@gmail.com>
2022-08-31 17:42:59 -0300

    continuation

--- registerForm/registerFormApp/src/app/app.module.ts
+++ registerForm/registerFormApp/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,11 +9,13 @@ import { RegisterformComponent } from './registerform/registerform.component';
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterformComponent
+    RegisterformComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- registerForm/registerFormApp/src/app/registerform/registerform.component.ts
+++ registerForm/registerFormApp/src/app/registerform/registerform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registerform',
@@ -8,8 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterformComponent implements OnInit {
 
   constructor() { }
-
+  registerForm:any //form
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      "firstName": new FormControl(null,[Validators.required,Validators.pattern('[a-zA-Z ]*')]),
+      "lastName": new FormControl(null),
+      "emailId": new FormControl(null),
+      "mobileNumber": new FormControl(null),
+    })
   }
-
+  // submit funcion
+  submitData()
+  {
+    console.log( this.registerForm.value );
+  }
+  get firstName() {return this.registerForm.get('firstName'); }
 }
