25c8210149e118169429cd53ff193e9ffb095293
alexsteeve <alexsteeve@gmail.com>
2022-09-05 08:46:56 -0300

    project finished

--- registerForm/registerFormApp/src/app/registerform/registerform.component.ts
+++ registerForm/registerFormApp/src/app/registerform/registerform.component.ts
@@ -13,15 +13,22 @@ export class RegisterformComponent implements OnInit {
   ngOnInit(): void {
     this.registerForm = new FormGroup({
       "firstName": new FormControl(null,[Validators.required,Validators.pattern('[a-zA-Z ]*')]),
-      "lastName": new FormControl(null),
-      "emailId": new FormControl(null),
-      "mobileNumber": new FormControl(null),
+      "lastName": new FormControl(null,[Validators.required,Validators.pattern('[a-zA-Z ]*')]),
+      "emailId": new FormControl(null,[Validators.required,Validators.email]),
+      "mobileNumber": new FormControl(null,[Validators.required,Validators.pattern('[0-9]*')]),
     })
   }
   // submit funcion
   submitData()
   {
     console.log( this.registerForm.value );
+    if(this.registerForm.valid){
+      alert(`Thank you ${this.registerForm.value.firstName} `);
+      this.registerForm.reset(); // reset form
+    }
   }
-  get firstName() {return this.registerForm.get('firstName'); }
+  get firstName() {return this.registerForm.get('firstName'); } // firstName
+  get lastName() {return this.registerForm.get('lastName'); } // lastName
+  get emailId() {return this.registerForm.get('emailId'); } // emaiId
+  get mobileNumber() {return this.registerForm.get('mobileNumber'); } // mobileNumber
 }
